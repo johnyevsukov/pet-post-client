946d9f7370cd63b4f28c7275e5e66b463fbd3641
John Yevsukov <johnyevsukov@Johns-MacBook-Pro.local>
2023-07-12 20:57:40 -0400

    base routes / pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "./components/pages/Login/Login";
+import { Signup } from "./components/pages/Signup/Signup";
+import { NotFound } from "./components/pages/NotFound/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<div>home</div>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
